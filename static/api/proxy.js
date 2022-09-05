364b7544fbcf28fb11d92fa72ef0b2760d77dbf4
Tehuel <torresbaldi.tehuel@gmail.com>
2022-09-05 19:20:45 -0300

    agrego proxy

---
+++ static/api/proxy.js
@@ -0,0 +1,17 @@
+export default function handler(req, res, next) {
+  async function getURI(url) {
+    try {
+      const response = await axios.get(url);
+      if (response.status !== 200) {
+        return res.status(response.status).json({ type: 'error', message: response.statusText });
+      } else {
+      res.json(response.data);
+      }
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ type: 'error', message: error.message });
+    }
+  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  getURI(req.body['https://coffee.alexflipnote.dev/random.json'])
+}
